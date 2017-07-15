b08fa6350f4d7f2465d95c4c0c5cecf1e34c7564
Carlos Rodríguez <carlos@Midgard.localdomain>
2017-07-15 18:45:22 -0500

    Adding formulas

--- dipole_field.c
+++ dipole_field.c
@@ -3,14 +3,7 @@
 
 float calculate_magnetic_field(float m, float theta)
 {
-	float B;
-	return B;
-}
-
-int main()
-{
-
-	float m, theta, r, Br, Bt, Bp, B;
+	float r, Br, Bt, Bp, B;
 	m = 1;
 	printf("Ingresa theta ");
 	scanf("%f",&theta);
@@ -20,6 +13,27 @@ int main()
 	Bt = (2*m*cos(theta)) / pow(r,3);
 	Bp = 0;
 	B = sqrt( pow(Br,2) + pow(Bt,2) + pow(Bp,2) );
-	printf("Br = %f\n Bt = %f\n Bp = %f\n B = %f",Br,Bt,Bp,B);
+	return B;
+}
+
+void polar_to_cartesian(float theta, float r, float *xptr, float *yptr)
+{
+	float x, y;
+	x = r * pow(sin(theta),2) * cos(theta);
+	y = r * pow(sin(theta),3);
+	*xptr = x;
+	*yptr = y;
+}
+
+int main()
+{
+	float theta, r, x, y, r1;
+	r1 = .2;
+	for(theta = 0; theta <= 6.28; theta += 1)
+	{
+		r = r1 * pow( sin(theta), 2);
+		polar_to_cartesian(theta,r1,&x,&y);
+		printf("theta = %f\nr = %f\nX = %f\nY = %f\n\n",theta,r,x,y);
+	}
 	return 0;
 }
